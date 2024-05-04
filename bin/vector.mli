2f5197f93dac008da6bb4aa60f5b8ad73ca17c19
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-05-04 01:55:17 -0400

    interface functions for vector

---
+++ bin/vector.mli
@@ -0,0 +1,10 @@
+(* dynamicArray.mli *)
+
+type t
+
+val create : unit -> t
+val add : t -> int -> unit
+val get : t -> int -> int
+val set : t -> int -> int -> unit
+val is_empty: t -> bool
+val length : t -> int
