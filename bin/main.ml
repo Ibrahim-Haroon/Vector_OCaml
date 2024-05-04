6f650dc1af4c3b330d0cce94ceaefd55f71bc709
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-05-04 02:26:24 -0400

    sample of using vector

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,10 @@
+(* main.ml *)
+open Vector
 
 let () =
-	print_endline "Hello, World!"
+    let vector = create () in
+    add vector 10;
+    add vector 20;
+    print_endline ("Element at index 1: " ^ string_of_int (get vector 0));
+    set vector 1 30;
+    print_endline ("Length of vector: " ^ string_of_int (length vector))
